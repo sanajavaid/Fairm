7895af2c8041aa5bf378c2f18eace04923550156
sana javaid <sanajavaid5223@gmail.com>
2024-08-21 20:20:19 +0500

    commit

--- app/src/main/java/com/example/fairm/MainActivity.kt
+++ app/src/main/java/com/example/fairm/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             FairmTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Sana",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
